c96889908e4882ae1ee544d2dad1b5c1ff22325a
Eje <eje.nathaniel@gmail.com>
2023-02-13 15:44:10 +0100

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+const API_KEY = "e1373c69ca6dc77ac7692ffc2240d3f7";
+const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=&units=metric&APPID=`;
 
 export default function App() {
+
+  const [location, setLocation] = useState('');
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+
+  const getWeatherFromApi = () => {
+    setLoading(true)
+    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=${API_KEY}`)
+      .then(response => response.json())
+      .then(json => {
+        setData(json);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    getWeatherFromApi();
+  }, [location])
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* {console.log(data)} */}
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Location"
+          onSubmitEditing={(value) => setLocation(value.nativeEvent.text)}
+          maxLength={20}
+          minLength={2}
+        />
+      </View>
+      {!data.length > 0 ?
+
+        (<Text> Welcome </Text>)
+
+        : (
+          <View >
+            {console.log(data)}
+            <Text style={styles.heading}>Today Weather</Text>
+            <Text style={styles.country}>{data.name},</Text>
+            <Text style={styles.temp}>{data.main.temp}33°C</Text>
+          </View>
+        )}
+
     </View>
   );
 }
@@ -13,8 +61,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 80,
+    padding: 30,
+    backgroundColor: 'skyblue',
+
+  },
+  form: {
+    alignItems: 'center',
+    marginBottom: 60,
+  },
+  input: {
+    borderColor: "gray",
+    width: "100%",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    color: 'gray',
+  },
+  heading: {
+    fontSize: 32,
+    fontWeight: 100,
+    marginBottom: 20,
+    color: 'white',
+  },
+  country: {
+    fontSize: 22,
+    marginBottom: 20,
+    color: 'white',
+  },
+  temp: {
+    fontSize: 88,
+    color: 'white',
   },
 });
